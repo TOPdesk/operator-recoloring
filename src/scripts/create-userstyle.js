6c2e487c43b190106c83389be385efea0a77bba0
Joppe Kroon <J.Kroon@topdesk.com>
2021-01-15 12:10:08 +0100

    #7 Give parent card info blocks surface style

--- src/scripts/create-userstyle.js
+++ src/scripts/create-userstyle.js
@@ -58,6 +58,7 @@ export default function createUserstyle(version) {
   ${readFileSync(__dirname + '/src/styles/knowledge-item.css', {encoding: 'utf8'})}
   ${readFileSync(__dirname + '/src/styles/email.css', {encoding: 'utf8'})}
   ${readFileSync(__dirname + '/src/styles/selection.css', {encoding: 'utf8'})}
+  ${readFileSync(__dirname + '/src/styles/card.css', {encoding: 'utf8'})}
 }
 `
       this.emitFile({
